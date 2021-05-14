7b46c82942a7fb01066d877f83c1319de5e85d38
Cristina Plesa <cristinaplesa8@gmail.com>
2021-05-14 14:40:48 -0700

    WIP: switch gears to work in Order functionality to try and debug last Vendor test

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -97,7 +97,10 @@ namespace VendorTracker.Tests
     {
      //Arrange
       string title = "Colac Order";
-      Order newOrder = new(title);
+      string description = "Joann orders 9 Colac loaves every other day. This is a reoccuring order.";
+      int price = 30;
+      string date = "05/14/2021";
+      Order newOrder = new(title, description, price, date);
       List<Order> newOrderList = new List<Order> { newOrder };
       string name = "Joann's Coffee";
       string description = "A Porland-based cart cafe and seller of baked goods!";
--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -7,11 +7,11 @@ namespace VendorTracker.Models
     public string Title { get ; set ; }
     public string Description { get ; set ; }
     public int Price { get ; }
-    public int Date { get ; }
+    public string Date { get ; }
     public int Id { get ; }
     private static List<Order> _instances = new List<Order> {};
 
-    public Order(string title, string description, int price, int date)
+    public Order(string title, string description, int price, string date)
     {
       Title = title;
       Description = description;
