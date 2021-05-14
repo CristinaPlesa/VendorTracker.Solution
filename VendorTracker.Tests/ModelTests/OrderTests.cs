863fd656fb774a7ec6c50c0727ab6e8e2a5f28e2
Cristina Plesa <cristinaplesa8@gmail.com>
2021-05-14 15:48:28 -0700

    add test for return of an Order List, Green Phase passed

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -90,5 +90,26 @@ namespace VendorTracker.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrdersList_OrderList()
+    {
+      //Arrange
+      string title01 = "Colac Order";
+      string description01 = "Joann orders 9 Colac loaves every other day. This is a reoccuring order.";
+      int price01 = 30;
+      string date01 = "05/14/2021";
+      string title02 = "Galuste cu Prune Order";
+      string description02 = "Joann orders 15 Galuste pastries every other day. This is a reoccuring order.";
+      int price02 = 25;
+      string date02 = "05/14/2021";
+      Order newOrder1 = new(title01, description01, price01, date01);
+      Order newOrder2 = new(title02, description02, price02, date02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      //Act
+      List<Order> result = Order.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
