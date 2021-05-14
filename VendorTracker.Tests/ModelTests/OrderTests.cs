165b96485b6981cd11363bc99e922f8557611853
Cristina Plesa <cristinaplesa8@gmail.com>
2021-05-14 16:25:15 -0700

    add GetId test for Order constructor, Green Phase passed

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -111,5 +111,20 @@ namespace VendorTracker.Tests
       //Assert
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string title = "Colac Order";
+      string description = "Joann orders 9 Colac loaves every other day. This is a reoccuring order.";
+      int price = 30;
+      string date = "05/14/2021";
+      Order newOrder = new(title, description, price, date);
+      //Act
+      int result = newOrder.Id;
+      //Assert
+      Assert.AreEqual(1, result);
+    }
   }
 }
