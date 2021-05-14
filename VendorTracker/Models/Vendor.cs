bc018d6493a8b7e0f443836a28756eac2a5379c9
Cristina Plesa <cristinaplesa8@gmail.com>
2021-05-14 10:16:32 -0700

    add Vendor constructor test and function and ClearAll method, Red phase passed

---
+++ VendorTracker/Models/Vendor.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace VendorTracker.Models
+{
+  public class Vendor
+  {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get ; set ; }
+    public string Description { get ; set ; }
+    public int Id { get ; }
+    public List<Vendor> Vendors { get ; set ; }
+
+    public Vendor()
+    {
+
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+  }
+}
